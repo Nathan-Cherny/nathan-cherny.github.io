157d2209a08f8af22752945e1fc5253ae127e14f
Nathan Cherny <natec3632@gmail.com>
2026-07-24 12:09:24 -0400

    rewriting navbar update

--- updatenavfooter.py
+++ updatenavfooter.py
@@ -443,8 +443,13 @@ for fileName in file_paths:
     with open(fileName, "r", encoding="utf-8") as file:
         htmlContent = file.read()
 
-        print(path)
-        print([i for i in htmlContent.split("\n") if f"<a href=\"/{path}/\"" in i])
+        contentSplit = htmlContent.split("\n")
+        l = [contentSplit.index(i) - 2 for i in contentSplit if f"<a href=\"/{path}/\"" in i]
+        a = [i for i in l if "menu-item" in contentSplit[i]]
+        if len(a) == 0: continue
+        contentSplit[a[0]] = contentSplit[a[0]].replace("class=\"", "class=\"current-menu-item ")
+
+        htmlContent = "\n".join(contentSplit)
 
         # headerFirstLine = """<header id="site-header" class="header-footer-group">"""
         # header = headerFirstLine + htmlContent.split("<!-- #site-header -->")[0].split(headerFirstLine)[1]
@@ -458,5 +463,5 @@ for fileName in file_paths:
         
         # htmlContent = htmlContent.replace(footer, newFooter)
 
-    # with open(fileName, "w", encoding="utf-8") as file:
-    #     file.write(htmlContent)
+    with open(fileName, "w", encoding="utf-8") as file:
+        file.write(htmlContent)
