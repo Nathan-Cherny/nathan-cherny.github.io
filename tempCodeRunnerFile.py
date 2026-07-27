afa5a5619feb27b12bc9d5399fa9cf3ab6dc2401
Nathan Cherny <natec3632@gmail.com>
2026-07-27 10:51:59 -0400

    updating header with new logo

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -433,18 +433,18 @@ for fileName in file_paths:
     with open(fileName, "r", encoding="utf-8") as file:
         htmlContent = file.read()
 
-        contentSplit = htmlContent.split("\n")
-        l = [contentSplit.index(i) - 2 for i in contentSplit if f"<a href=\"/{path}/\"" in i]
-        a = [i for i in l if "menu-item" in contentSplit[i]]
-        if len(a) == 0: continue
-        contentSplit[a[0]] = contentSplit[a[0]].replace("class=\"", "class=\"current-menu-item ")
+        # contentSplit = htmlContent.split("\n")
+        # l = [contentSplit.index(i) - 2 for i in contentSplit if f"<a href=\"/{path}/\"" in i]
+        # a = [i for i in l if "menu-item" in contentSplit[i]]
+        # if len(a) == 0: continue
+        # contentSplit[a[0]] = contentSplit[a[0]].replace("class=\"", "class=\"current-menu-item ")
 
-        htmlContent = "\n".join(contentSplit)
+        # htmlContent = "\n".join(contentSplit)
 
-        # headerFirstLine = """<header id="site-header" class="header-footer-group">"""
-        # header = headerFirstLine + htmlContent.split("<!-- #site-header -->")[0].split(headerFirstLine)[1]
+        headerFirstLine = """<header id="site-header" class="header-footer-group">"""
+        header = headerFirstLine + htmlContent.split("<!-- #site-header -->")[0].split(headerFirstLine)[1]
 
-        # htmlContent = htmlContent.replace(header, newHeader)
+        htmlContent = htmlContent.replace(header, newHeader)
 
         # footerFirstLine = "<!-- #site-content -->"
         # footerLastLine = "<!-- #site-footer -->"
