da54bf73c8474e6523f4ff260fc96229fdb54b4a
Nathan Cherny <natec3632@gmail.com>
2026-08-05 15:24:41 -0400

    prototype page first draft

--- updatenavfooter.py
+++ updatenavfooter.py
@@ -1033,20 +1033,20 @@ for fileName in file_paths:
         htmlContent = file.read()
 
         # -- CURRENT-MENU-ITEM --
-        contentSplit = htmlContent.split("\n")
-        l = [
-            contentSplit.index(i) - 2
-            for i in contentSplit
-            if f'<a href="/{path}/"' in i
-        ]
-        a = [i for i in l if "menu-item" in contentSplit[i]]
-        if len(a) == 0:
-            continue
-        contentSplit[a[0]] = contentSplit[a[0]].replace(
-            'class="', 'class="current-menu-item '
-        )
+        # contentSplit = htmlContent.split("\n")
+        # l = [
+        #     contentSplit.index(i) - 2
+        #     for i in contentSplit
+        #     if f'<a href="/{path}/"' in i
+        # ]
+        # a = [i for i in l if "menu-item" in contentSplit[i]]
+        # if len(a) == 0:
+        #     continue
+        # contentSplit[a[0]] = contentSplit[a[0]].replace(
+        #     'class="', 'class="current-menu-item '
+        # )
 
-        htmlContent = "\n".join(contentSplit)
+        # htmlContent = "\n".join(contentSplit)
 
         # -- HEADER --
         # headerFirstLine = """<header id="site-header" class="header-footer-group">"""
