a4cb39ddfd5b0f1b5d7e1357039fa245c1139982
Nathan Cherny <natec3632@gmail.com>
2026-08-06 14:05:25 -0400

    current menu item

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,14 @@
-        headerFirstLine = """<header id="site-header" class="header-footer-group">"""
-        header = (
-            headerFirstLine
-            + htmlContent.split("""<main id="site-content">""")[0].split(headerFirstLine)[1]
+        contentSplit = htmlContent.split("\n")
+        l = [
+            contentSplit.index(i) - 2
+            for i in contentSplit
+            if f'<a href="/{path}/"' in i
+        ]
+        a = [i for i in l if "menu-item" in contentSplit[i]]
+        if len(a) == 0:
+            continue
+        contentSplit[a[0]] = contentSplit[a[0]].replace(
+            'class="', 'class="current-menu-item '
         )
 
-        htmlContent = htmlContent.replace(header, newHeader)
+        htmlContent = "\n".join(contentSplit)
