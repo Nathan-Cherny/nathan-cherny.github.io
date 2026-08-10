3d2784773f96b6aaa821e481b1478f8dfde896d8
Nathan Cherny <natec3632@gmail.com>
2026-08-10 12:24:57 -0400

    smaller size for prototyping section

--- updatenavfooter.py
+++ updatenavfooter.py
@@ -1058,20 +1058,20 @@ for fileName in file_paths:
         # htmlContent = htmlContent.replace(header, newHeader)
 
         # -- CURRENT-MENU-ITEM --
-        contentSplit = htmlContent.split("\n")
-        l = [
-            contentSplit.index(i) - 2
-            for i in contentSplit
-            if f'<a href="/{path}/"' in i
-        ]
-        a = [i for i in l if "menu-item" in contentSplit[i]]
-        if len(a) == 0:
-            continue
-        contentSplit[a[0]] = contentSplit[a[0]].replace(
-            'class="', 'class="current-menu-item '
-        )
+        # contentSplit = htmlContent.split("\n")
+        # l = [
+        #     contentSplit.index(i) - 2
+        #     for i in contentSplit
+        #     if f'<a href="/{path}/"' in i
+        # ]
+        # a = [i for i in l if "menu-item" in contentSplit[i]]
+        # if len(a) == 0:
+        #     continue
+        # contentSplit[a[0]] = contentSplit[a[0]].replace(
+        #     'class="', 'class="current-menu-item '
+        # )
 
-        htmlContent = "\n".join(contentSplit)
+        # htmlContent = "\n".join(contentSplit)
 
         # -- HEAD --
         # headFirstLine = """<head>"""
